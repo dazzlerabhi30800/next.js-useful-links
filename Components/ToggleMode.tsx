271d47b09e1137e669e8396228cf73aa819295a2
= <abhisingh30800@gmail.com>
2023-12-05 20:34:39 +0530

    added local storage for toggle light theme

--- Components/ToggleMode.tsx
+++ Components/ToggleMode.tsx
@@ -1,28 +1,40 @@
 "use client";
-import { SunIcon, MoonIcon } from "@heroicons/react/20/solid"
-import { useState } from 'react';
-
+import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
 
 export default function ToggleMode() {
-    // this will the set the theme for the website default is false i.e dark theme
-    const [mode, setMode] = useState<boolean>(false);
-    function handleMode() {
-        if (mode) {
-            setMode(prev => prev = false);
-            document.documentElement.classList.remove('theme-light');
-            return;
-        }
+  // this will the set the theme for the website default is false i.e dark theme
+  const [mode, setMode] = useState<boolean | null | string>(false);
 
-        setMode(prev => prev = true);
-        document.documentElement.classList.add('theme-light');
+
+  useEffect(() => {
+    const modeValue = typeof window !== 'undefined' && window.localStorage ? JSON.parse(window.localStorage.getItem('mode') || "") : null;
+    console.log(modeValue);
+    if (modeValue && window.localStorage) {
+      setMode(prev => prev = modeValue);
     }
-    return (
-        <button onClick={handleMode} className="mode-toggler">
-            {mode ?
-                <MoonIcon width={30} height={30} />
-                :
-                <SunIcon width={30} height={30} />
-            }
-        </button>
-    )
+  }, [])
+
+
+  useEffect(() => {
+    if (mode) {
+      document.documentElement.classList.add("theme-light");
+    }
+    else {
+      document.documentElement.classList.remove("theme-light");
+    }
+
+    window.localStorage.setItem('mode', JSON.stringify(mode));
+  }, [mode])
+
+
+  return (
+    <button onClick={() => setMode(prev => !prev)} className="mode-toggler">
+      {mode ? (
+        <MoonIcon width={30} height={30} />
+      ) : (
+        <SunIcon width={30} height={30} />
+      )}
+    </button>
+  );
 }
