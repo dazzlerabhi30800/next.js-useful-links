33f4d5cdf07223fb589fe76ec093308caabaeac5
= <abhisingh30800@gmail.com>
2023-12-07 12:41:21 +0530

    added certification section & added JS certification

--- Components/ToggleMode.tsx
+++ Components/ToggleMode.tsx
@@ -4,30 +4,30 @@ import { useEffect, useState } from "react";
 
 export default function ToggleMode() {
   // this will the set the theme for the website default is false i.e dark theme
-  const modeValue = typeof window !== "undefined" && window.localStorage.getItem("mode") ? JSON.parse(window.localStorage.getItem("mode") || "") : false;
+  const modeValue =
+    typeof window !== "undefined" && window.localStorage.getItem("mode")
+      ? JSON.parse(window.localStorage.getItem("mode") || "")
+      : false;
   const [mode, setMode] = useState<boolean | null | string>(modeValue);
   const [isClient, setIsClient] = useState<boolean>(false);
 
   useEffect(() => {
     setIsClient(true);
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (mode) {
       document.documentElement.classList.add("theme-dark");
-    }
-    else {
+    } else {
       document.documentElement.classList.remove("theme-dark");
     }
 
-    window.localStorage.setItem('mode', JSON.stringify(mode));
-  }, [mode])
-
-
+    window.localStorage.setItem("mode", JSON.stringify(mode));
+  }, [mode]);
 
   if (!isClient) return false;
   return (
-    <button onClick={() => setMode(prev => !prev)} className="mode-toggler">
+    <button onClick={() => setMode((prev) => !prev)} className="mode-toggler">
       {mode ? (
         <SunIcon width={30} height={30} />
       ) : (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from 'next'
-import { Playpen_Sans } from 'next/font/google'
-import './globals.css'
-import ToggleMode from '@/Components/ToggleMode'
+import type { Metadata } from "next";
+import { Playpen_Sans } from "next/font/google";
+import "./globals.css";
+import ToggleMode from "@/Components/ToggleMode";
 
-const playpen = Playpen_Sans({ subsets: ['latin'] })
+const playpen = Playpen_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Link Portfolio App',
-  description: 'It is a webstite which is collection of links of all usefull resources.',
-}
+  title: "Link Portfolio App",
+  description:
+    "It is a webstite which is collection of links of all usefull resources.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -22,5 +23,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
