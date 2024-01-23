9c715fe8853732fdc8614a4fa39b0b1739dde54b
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-01-23 11:13:30 +0530

    fixed the issue of json file write not formatted

--- app/api/links/route.ts
+++ app/api/links/route.ts
@@ -1,10 +1,11 @@
-import { promises as fs } from "fs";
+import { promises as fsr } from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
+import fs from "fs";
 
 const filePath = process.cwd() + "/data/links.json";
 export async function GET() {
-  const file = await fs.readFile(filePath, "utf-8");
+  const file = await fsr.readFile(filePath, "utf-8");
   const data = JSON.parse(file);
   return NextResponse.json(data);
 }
@@ -17,15 +18,13 @@ export async function GET() {
 export async function POST(req: Request) {
   const { key, name, link } = await req.json();
 
-  const file = await fs.readFile(filePath, "utf-8");
+  const file = await fsr.readFile(filePath, "utf-8");
   const data = JSON.parse(file);
   const newData = {
     name,
     link,
   };
   data[key].push(newData);
-  fs.writeFile(filePath, JSON.stringify(data));
-  // await fs.writeFile(data, updatedFile);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
   return NextResponse.json(data);
-  // return NextResponse.json(file);
 }
