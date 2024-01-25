ba5e056a3ec762d369c9d4c76d50bb827182d63c
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-01-25 23:32:08 +0530

    changed the function from delete to get static props

--- app/api/links/delete/route.ts
+++ app/api/links/delete/route.ts
@@ -3,7 +3,7 @@ import { linkInterface } from "@/type";
 import fs from "fs";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req: Request) {
+export async function getStaticProps(req: Request) {
   const { key, title } = await req.json();
   const file = fs.readFileSync(filePath, "utf-8");
   const data = JSON.parse(file);
