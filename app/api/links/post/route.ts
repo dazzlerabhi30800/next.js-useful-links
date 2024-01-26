5f4de62a28b3e82e7c030f75d860806ad8947741
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-01-26 11:22:03 +0530

    testing the post endpoint #3

--- app/api/links/post/route.ts
+++ app/api/links/post/route.ts
@@ -1,16 +1,18 @@
 import { NextResponse } from "next/server";
+import fs from "fs";
 
+const filePath = process.cwd() + "/data/links.json";
 export async function POST(req: Request) {
   const { key, name, link } = await req.json();
 
-  // const file = fs.readFileSync(filePath, "utf-8");
-  // const data = JSON.parse(file);
-  // const newData = {
-  //   name,
-  //   link,
-  // };
+  const file = fs.readFileSync(filePath, "utf-8");
+  const data = JSON.parse(file);
+  const newData = {
+    name,
+    link,
+  };
 
-  // data[key].push(newData);
-  // fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  data[key].push(newData);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
   return NextResponse.json({ name, link });
 }
